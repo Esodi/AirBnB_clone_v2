f9b0fc6f1200abfe7bcf60c9fdc6027dfa14cc8a
DaBetterman <1ebrahimr@gmail.com>
2024-03-19 17:39:50 +0200

    removed pass, added self.save

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,8 +58,11 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        try:
-            del self.__objects["{}:{}".format(type(obj).__name__, obj.id)]
-            
-        except (AttributeError, KeyError):
-            pass
+        """
+        Delete obj from __objects if it's inside.
+        """
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
